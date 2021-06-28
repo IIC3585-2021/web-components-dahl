afaf30036bca013de0f0303ca07625c48d7779c5
nabenitez <nabenitez@uc.cl>
2021-06-27 20:50:45 -0400

    update: name

--- to-do-list/src/index.js
+++ to-do-list/src/index.js
@@ -1,7 +1,7 @@
-import MyTodo from "./my-todo";
+import Todo from "./my-todo";
 import TodoInput from "./todo-input";
 import TodoItem from "./todo-item";
 
-window.customElements.define('todo-list', MyTodo);
+window.customElements.define('todo-list', Todo);
 window.customElements.define('todo-input', TodoInput);
 window.customElements.define('todo-item', TodoItem);
